c08dc41385e66f41e395391ab824faace1e272ed
surendra kumar <muchiramsurendrakumar.1992@gmail.com>
2022-10-10 22:14:13 +0530

    added nexted components inside app component added employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{EmployeeComponent} from './employee/employee.component'
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmployeeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,14 @@
+import{Component} from '@angular/core'
+
+@Component({
+    selector: 'my-employee',
+    templateUrl: './employee.component.html',
+    styleUrls:['./employee.component.css']
+})
+export class EmployeeComponent
+{
+    firstName: string = 'Tom';
+    lastName: string = 'Hopkins';
+    gender: string = 'Male';
+    age: number = 20;
+}
